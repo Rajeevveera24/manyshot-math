3b0600ddeb940b3e924806062d0bd7ef5f59b621
anishkulkarni <49675971+anishkulkarni@users.noreply.github.com>
2024-11-23 22:02:15 -0500

    Added class for random example retriever for ICL for GSM8K dataset

---
+++ retrievers/random_retriever_gsm8k.py
@@ -0,0 +1,23 @@
+# External imports
+import numpy as np
+
+# Internal imports
+from random_retriever import RandomRetriever
+
+class RandomRetrieverGSM8K(RandomRetriever):
+
+    def __init__(self, data_path):
+        super().__init__(data_path)
+
+    def count_data(self):
+        self.instances = len(self.questions)
+
+    def organize_data(self):
+        self.questions = self.dataset["question"]
+        self.answers = self.dataset["extracted_answers"]
+
+    def retrieve(self, n_examples):
+        selected_indices = np.random.choice(self.instances, (n_examples), replace=False)
+        selected_questions = np.array(self.questions)[selected_indices].tolist()
+        selected_answers = np.array(self.answers)[selected_indices].tolist()
+        return selected_questions, selected_answers
