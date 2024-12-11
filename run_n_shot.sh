d943cec0e4bed98ec350f6690dd27d67a3689e24
ajaymittur <amittur@andrew.cmu.edu>
2024-12-10 22:18:22 -0500

    Add 25, 75, 125 results

--- run_n_shot.sh
+++ run_n_shot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=inf_math_eval
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:A100_40GB:1
+#SBATCH --gres=gpu:L40S:1
 #SBATCH --mem=64G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=amittur@cs.cmu.edu
